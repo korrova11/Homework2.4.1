05752c7fe225e70424b3c45ef9c4b50282b08cce
korrova11 <korrova@yandex.ru>
2023-11-23 00:58:21 +0300

    second commit

--- src/test/java/pro/sky/java/course2/calculator/CalculatorServiceImplParametrizedTest.java
+++ src/test/java/pro/sky/java/course2/calculator/CalculatorServiceImplParametrizedTest.java
@@ -11,33 +11,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorServiceImplParametrizedTest {
     private final CalculatorServiceImpl out = new CalculatorServiceImpl();
-    public static Stream<Arguments> provideParamsForTest(){
+
+    public static Stream<Arguments> provideParamsForTest() {
         return Stream.of(
-        Arguments.of(4,2),
-        Arguments.of(12,3));
+                Arguments.of(4, 2),
+                Arguments.of(12, 3));
     }
+
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
-    public void paramsSummaTest(int num1,int num2){
-        assertTrue( out.summa(num1,num2).contains(""+(num1+num2)));
+    public void paramsSummaTest(int num1, int num2) {
+        assertTrue(out.summa(num1, num2).contains("" + (num1 + num2)));
 
     }
+
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
-    public void paramsMinusTest(int num1,int num2){
-        assertTrue( out.minus(num1,num2).contains(""+(num1-num2)));
+    public void paramsMinusTest(int num1, int num2) {
+        assertTrue(out.minus(num1, num2).contains("" + (num1 - num2)));
 
     }
+
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
-    public void paramsMultiplyTest(int num1,int num2){
-        assertTrue( out.multiply(num1,num2).contains(""+(num1*num2)));
+    public void paramsMultiplyTest(int num1, int num2) {
+        assertTrue(out.multiply(num1, num2).contains("" + (num1 * num2)));
 
     }
+
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
-    public void paramsDivideTest(int num1,int num2){
-        assertTrue( out.divide(num1,num2).contains(""+(num1/num2)));
+    public void paramsDivideTest(int num1, int num2) {
+        assertTrue(out.divide(num1, num2).contains("" + (num1 / num2)));
 
     }
 }
