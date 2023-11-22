3f7dbf0644f8ba57933fb5f63cf4a09547995d09
korrova11 <korrova@yandex.ru>
2023-11-22 15:15:25 +0300

    first commit

--- src/main/java/pro/sky/java/course2/calculator/CalculatorApplication.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorApplication.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class CalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
 
-	}
+}
--- src/main/java/pro/sky/java/course2/calculator/service/impl/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/impl/CalculatorServiceImpl.java
@@ -1,8 +1,8 @@
-package pro.sky.java.course2.calculator;
+package pro.sky.java.course2.calculator.service.impl;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import pro.sky.java.course2.calculator.service.api.CalculatorService;
+
 @Service
 
 public class CalculatorServiceImpl implements CalculatorService {
@@ -26,7 +26,7 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     public String divide(int usnum1, int usnum2) {
         if (usnum2 == 0) {
-            return "Ошибка: на ноль делить нельзя!";}
+            throw new IllegalArgumentException("На ноль делить нельзя!");}
 
         else
                 return usnum1 + " / " + usnum2 + " = " + (usnum1 / usnum2);
